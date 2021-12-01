e952a7f98e1eaa47f28a7ca75aad6ecf24c4335a
KapteinKS <sander.toien@outlook.com>
2021-12-01 16:09:26 +0100

    update mapsactivity

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -92,12 +92,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 getAddress(latLng);
             }
         });
-
-
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 
